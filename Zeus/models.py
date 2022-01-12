9c72165b218c8f0e524898353e48458385c65cc1
spralja <robert.spralja@gmail.com>
2022-01-12 15:06:40 +0100

    created test event model

--- Zeus/models.py
+++ Zeus/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class Event(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    def clean(self):
+        super().clean()
+        if self.end_time <= self.start_time:
+            raise forms.ValidationError("End time must be after start time!")
